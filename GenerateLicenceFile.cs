0ee7f1acb53246f03bd90096dfbaa8abd677924d
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-07-20 23:48:13 +0100

    First commit

---
+++ GenerateLicenceFile.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace pluralsightfuncs
+{
+    public static class GenerateLicenceFile
+    {
+        [FunctionName("GenerateLicenceFile")]
+        public static async Task Run(
+            [QueueTrigger("orders", Connection = "AzureWebJobsStorage")] Order order,
+            IBinder binder,
+            ILogger log)
+        {
+            var outputBlob = await binder.BindAsync<TextWriter>(
+                new BlobAttribute($"licenses/{order.OrderId}.lic")
+                {
+                    Connection = "AzureWebJobsStorage"
+                });
+
+            outputBlob.WriteLine($"OrderId: {order.OrderId}");
+            outputBlob.WriteLine($"Email: {order.Email}");
+            outputBlob.WriteLine($"ProductId: {order.ProductId}");
+            outputBlob.WriteLine($"PurchaseDate: {DateTime.UtcNow}");
+            var md5 = System.Security.Cryptography.MD5.Create();
+            var hash = md5.ComputeHash(
+                System.Text.Encoding.UTF8.GetBytes(order.Email + "secret"));
+            outputBlob.WriteLine($"SecretCode: {BitConverter.ToString(hash).Replace("-", "")}");
+        }
+    }
+}
